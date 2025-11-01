03966b76830427b01fac894e4880b7eb9a733945
Osman Semir <osmanshek@gmail.com>
2025-11-01 21:43:39 +0400

    Add Gradient Background

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import "./App.css";
 function App() {
   return (
     <>
+      <div className="min-h-screen w-full bg-[#020617] relative">
+        {/* Magenta Orb Grid Background */}
+        <div className="absolute inset-0 z-0 custom-gradient" />
+        {/* Your Content/Components */}
     </>
   );
 }
