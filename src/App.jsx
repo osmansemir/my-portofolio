cd7076da8f51db3b97722eb7bea36ef9442d2441
Osman Semir <osmanshek@gmail.com>
2025-11-01 21:47:27 +0400

    Add Hero Section with Spotlight

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import { Spotlight } from "@/components/ui/Spotlight";
 function App() {
   return (
     <>
@@ -6,6 +8,16 @@ function App() {
         {/* Magenta Orb Grid Background */}
         <div className="absolute inset-0 z-0 custom-gradient" />
         {/* Your Content/Components */}
+        <div className="absolute inset-0 overflow-hidden">
+          <Spotlight
+            className="-top-30 z-100 left-0 md:-top-50 md:left-30"
+            fill="white"
+          />
+        </div>
+        <main className="relative max-w-xl mx-auto pb-3 px-5">
+          <Hero />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+function Hero() {
+  return (
+    <section
+      id="hero"
+      className="relative flex justify-center items-center z-30 pt-50 pb-20"
+    >
+      <div>
+        <h1 className="inline-block text-white text-5xl font-bold tracking-tighter md:text-7xl">
+          Hi I'm Osman
+        </h1>
+        <p className="text-gray-400 inline-block max-w-[600px] md:text-xl">
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
