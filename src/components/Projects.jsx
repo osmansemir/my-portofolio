a63dbf98366e031368eea6564fc77c7300f39933
Osman Semir <osmanshek@gmail.com>
2025-11-03 14:13:16 +0400

    Imporve Projects components and add project data

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,18 @@
-import ProjectCard from "./ProjectCard";
-import SectionTitle from "./SectionTitle";
+import ProjectCard from "@/components/ProjectCard";
+import SectionTitle from "@/components/SectionTitle";
+import { projects } from "@/data/projectsData";
 
 function Projects() {
   return (
     <section className="text-white relative py-10 z-30 flex flex-col">
       <SectionTitle>Projects</SectionTitle>
+      <h2 className="text-3xl text-center font-bold tracking-tighter mb-10 sm:text-5xl">
+        Check out my latest work
+      </h2>
       <div className="grid grid-cols-2 gap-3">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects.map((item) => (
+          <ProjectCard key={item.title} project={item} />
+        ))}
       </div>
     </section>
   );
---
+++ src/data/projectsData.jsx
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    title: "Mindvale blog",
+    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officiis illo, quia, quibusdam natus cumque rerum quod quae",
+    tech: ["react", "nextjs", "tailwindcss", "typescript", "sanity"],
+    source: "www.github.com",
+    website: "www.osmansemir.com",
+  },
+];
